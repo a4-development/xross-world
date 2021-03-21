6a93a3e36fabef01376bf750361cd9b9032622cc
Jun Koyama <cocor.au.lait@gmail.com>
2021-03-21 14:51:50 +0900

    Add filtering

--- src/utils/api/mapper/create-text-dictionary.ts
+++ src/utils/api/mapper/create-text-dictionary.ts
@@ -1,5 +1,8 @@
 import { ScannedImage, TextField } from '../processor/process-ocr'
 
+const MIN_TEXT_LENGTH = 3
+const MAX_TEXT_LENGTH = 10
+
 export type TextDictionary = Record<
   number,
   Pick<TextField, 'inferText' | 'inferConfidence' | 'boundingPoly'>
@@ -19,6 +22,12 @@ export default (images: ScannedImage[]): TextDictionary =>
       (text, index, self) =>
         index === self.findIndex(t => t.inferText === text.inferText)
     ) // Remove duplicate elements
+    .filter(
+      text =>
+        MIN_TEXT_LENGTH < text.inferText.length &&
+        text.inferText.length < MAX_TEXT_LENGTH
+    ) // Remove short or long texts
+    .filter(text => !/\d/.test(text.inferText)) // Remove texts contains number
     .reduce(
       (accumulator, current, index) => ({ ...accumulator, [index]: current }),
       {}
